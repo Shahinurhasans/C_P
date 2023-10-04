98ff0fbea5c4533118c7e7b323e3f867403734ed
Shahinurhasans <95462455+Shahinurhasans@users.noreply.github.com>
2023-10-05 00:32:08 +0600

    Update selectionSort.h

--- sorting-algorithms-master/selectionSort.h
+++ sorting-algorithms-master/selectionSort.h
@@ -1,26 +1,51 @@
-#include <vector>
+#include <iostream>
 
 using namespace std;
 
-// SELECTION SORT
-//------------------------------------------------------- 
+void selection(int arr[], int n)
+{
+    int i, j, small;
 
-void selectionSort(vector <int> &v, int n) {
-	int minPosition, aux;
+    for (i = 0; i < n-1; i++)    // One by one move boundary of unsorted subarray
+    {
+        small = i; //minimum element in unsorted array
 
-	for (int i = 0; i < n - 1; i++) {
-		minPosition = i; // suppose "i" is the minimum
+        for (j = i+1; j < n; j++)
+        if (arr[j] < arr[small])
+            small = j;
+// Swap the minimum element with the first element
+    int temp = arr[small];
+    arr[small] = arr[i];
+    arr[i] = temp;
+    }
+    cout<<"the minimum value is:   "<<arr[0]<<endl;
+    cout<<"the minimum value is:   "<<arr[n-1] <<endl;
+    }
 
-		for (int j = i + 1; j < n; j++) { // find the "min" element in the unsorted part
-			
-			if (v[j] < v[minPosition]) { 
-				minPosition = j;
-			}
-		}
-		//  swap the found "min" element to the sorted part
-		if (minPosition > i) { 
-			swap(v[minPosition], v[i]);
-		}
-	}
 
+void printArr(int a[], int n) /* function to print the array */
+{
+    int i;
+    for (i = 0; i < n; i++)
+        cout<< a[i] <<" ";
+	cout<<endl;
+}
+
+int main()
+{
+    int n;
+    cout<<"enter the size of array"<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"enter the element"<<endl;
+    for( int i=0;i<n;i++)
+    {
+    cin>>a[i];
+    }
+    cout<< "Before sorting array elements are - "<<endl;
+    printArr(a, n);
+    selection(a, n);
+    cout<< "\nAfter sorting array elements are - "<<endl;
+
+    return 0;
 }
