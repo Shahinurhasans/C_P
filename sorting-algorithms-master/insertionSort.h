1cae61428a13f4df799ecf669b9b74148d7f6bd5
Shahinurhasans <95462455+Shahinurhasans@users.noreply.github.com>
2023-10-05 00:38:33 +0600

    Update insertionSort.h

--- sorting-algorithms-master/insertionSort.h
+++ sorting-algorithms-master/insertionSort.h
@@ -1,26 +1,49 @@
-#include <vector>
+#include <iostream>
 
 using namespace std;
 
-// INSERTION SORT
-//------------------------------------------------------------
+void insertion(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
 
-void insertionSort(vector <int> &v, int n) {
-	int current, pos;
+        // Move elements of arr[0..i-1] that are greater than key
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
 
-	for (int i = 1; i < n; i++) {
-		current = v[i]; 
-		pos = i; // limit of the ordered part, pos not included
-
-		// we make space
-		while (pos > 0 && v[pos - 1] > current) {
-			v[pos] = v[pos - 1]; 
-			pos--;
-		}
-		
-		// we move the current value to its position 
-		if (pos != i)  
-			v[pos] = current; 
-	}
+        arr[j + 1] = key;
+    }
 }
 
+void printArr(int a[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of array: " << endl;
+    cin >> n;
+    int a[n];
+    cout << "Enter the elements: " << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cout << "Before sorting, array elements are: " << endl;
+    printArr(a, n);
+
+    insertion(a, n);
+
+    cout << "\nAfter sorting, array elements are: " << endl;
+    printArr(a, n);
+
+
+    cout<<"\n the minimum value is:   "<<a[0]<<endl;
+    cout<<"\n the maximum value is:   "<<a[n-1] <<endl;
+
+    return 0;
+}
